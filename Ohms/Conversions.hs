9252ee520dd41c3d284ddc2dbe0a77ae9a2fb3df
Harvey Bates <phenobottle@gmail.com>
2023-08-07 08:40:08 +1000

    initial commit

---
+++ Ohms/Conversions.hs
@@ -0,0 +1,42 @@
+module Ohms.Conversions where
+
+-- To Current
+fromVRtoI :: Float -> Float -> Float
+fromVRtoI v r = v / r
+
+fromPVtoI :: Float -> Float -> Float
+fromPVtoI p v = p / v
+
+fromPRtoI :: Float -> Float -> Float
+fromPRtoI p r = sqrt (p / r)
+
+-- To Volts
+fromPRtoV :: Float -> Float -> Float
+fromPRtoV p r = sqrt (p * r)
+
+fromPItoV :: Float -> Float -> Float
+fromPItoV p i = p / i
+
+fromIRtoV :: Float -> Float -> Float
+fromIRtoV i r = i * r
+
+-- To Resistance
+fromVItoR :: Float -> Float -> Float
+fromVItoR v i = v / i
+
+fromVPtoR :: Float -> Float -> Float
+fromVPtoR v p = (v**2) / p
+
+fromPItoR :: Float -> Float -> Float
+fromPItoR p i = p / (i**2)
+
+-- To Power
+fromVItoP :: Float -> Float -> Float
+fromVItoP v i = v * i
+
+fromIRtoP :: Float -> Float -> Float
+fromIRtoP i r = (i**2) / r
+
+fromVRtoP :: Float -> Float -> Float
+fromVRtoP v r = (v**2) / r
+
---
+++ ReversePolarity/main.hs
@@ -0,0 +1,47 @@
+-- To run:
+-- runghc -i.. main.hs
+
+import System.IO
+import Text.Printf
+import Text.Read (readMaybe)
+import System.Exit (exitFailure)
+import Ohms.Conversions
+
+getInput :: String -> IO (Float)
+getInput parameter = do
+  printf "What's the %s?\n" parameter
+  iv <- getLine
+  case readMaybe iv :: Maybe Float of
+    Just f -> return f
+    Nothing -> do
+      printf "Could not parse input.\n"
+      exitFailure
+
+main :: IO ()
+main = do 
+
+  -- Get the input voltage
+  input_voltage <- getInput "Input Voltage (in volts)"
+  
+  -- Get the max current
+  max_current <- getInput "Max Current (in amps)"
+
+  -- Choose a P-Channel Mosfet with a low Drain to Source Resistance (Rds ON)
+  -- Get the Rds ON of the mosfet
+  resistance <- getInput "Drain to Source Resistance (Rds ON) of the Mosfet (in ohms)"
+
+  -- Sizing the zener
+  zener_d <- getInput "Gate to Source Voltage (Vgs) of the Mosfet (in volts)"
+
+  printf "--- Results ---\n"
+  printf "Drain to Source Voltage (Vdss):\t>%.2f\tV\n" $ input_voltage * 1.33
+  printf "Current Continuous Drain (Id):\t>%.2f\tA\n" $ max_current * 1.33
+ 
+  -- Once a mosfet has been chosen check its Rds ON
+  let power = fromIRtoP max_current resistance
+  
+  --let power = (max_current ** 2) * resistance
+  printf "Mosfet power dissipation:\t%.2f\tW\n" power
+
+  printf "Zener Volage Nominal (Vz):\t<%.2f\tV\n" $ zener_d
+  
